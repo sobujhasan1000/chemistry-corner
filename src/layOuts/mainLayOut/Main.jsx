9b1464710b773822859433295776188d632b1bf6
mshipan <mshipan657@gmail.com>
2023-08-08 15:12:12 +0600

    Navbar Component Created

--- src/layOuts/mainLayOut/Main.jsx
+++ src/layOuts/mainLayOut/Main.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/shared/Navbar";
 
 const Main = () => {
-    return (
-        <div>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+    </div>
+  );
 };
 
 export default Main;
