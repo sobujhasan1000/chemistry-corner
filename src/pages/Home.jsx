0e4987fa6c9a86090c99c32e21b890a03d681ed1
moktubat <shadhinreja@gmail.com>
2023-08-09 00:54:56 +0600

    location added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Counter from "../components/Home/Counter";
 import PremiumMembar from "../components/Home/PremiumMembar";
 import CheckoutPartner from "../components/Home/CheckoutPartner";
+import Location from "../components/Home/Location";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <CheckoutPartner />
       <Counter></Counter>
       <PremiumMembar></PremiumMembar>
+      <Location></Location>
     </div>
   );
 };
