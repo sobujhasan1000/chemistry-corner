c68f873060f4464434bb9dae003f5a13e9fa8c24
unknown <sobujhasan1000@gmail.com>
2023-08-08 21:53:02 +0600

    premium membership add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Counter from "../components/Home/Counter";
+import PremiumMembar from "../components/Home/PremiumMembar";
 
 const Home = () => {
   return (
     <div>
       <Counter></Counter>
+      <PremiumMembar></PremiumMembar>
     </div>
   );
 };
