3fdb63edf50605136f4828e43d330f267b9df37f
simanto97 <saimumsimanto97@gmail.com>
2023-08-17 17:21:26 +0600

    members section successfully implemented

--- src/components/Members/Members.jsx
+++ src/components/Members/Members.jsx
@@ -1,26 +1,37 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Container from "../shared/Container";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import membersBg from "../../assets/membersBg.jpg";
 import { Helmet } from "react-helmet-async";
+import {
+  getAllMembers,
+  getGenderWiseMembers,
+  membersSearch,
+} from "../../api/fetch";
 
 const Members = () => {
   const [members, setMembers] = useState([]);
-  const searchRef = useRef(null);
   const [search, setSearch] = useState("");
 
+  useEffect(() => {
+    getAllMembers().then((data) => setMembers(data));
+  }, []);
+
   const handleMembers = (gender) => {
-    console.log("search", search);
-    getGenderWiseMembers(gender, search).then((data) => setMembers(data));
+    getGenderWiseMembers(gender).then((data) => setMembers(data));
   };
 
-  const genderLists = ["male", "female", "non-binary"];
+  const genderLists = [
+    { label: "all", value: "" },
+    { label: "male", value: "male" },
+    { label: "female", value: "female" },
+    { label: "non-binary", value: "non-binary" },
+  ];
 
   const handleSearch = () => {
-    // console.log(searchRef.current.value);
-    setSearch(searchRef.current.value);
+    membersSearch(search).then((member) => setMembers(member));
   };
   return (
     <div style={{ backgroundImage: `url(${membersBg})` }}>
@@ -48,7 +59,7 @@ const Members = () => {
                   />
                 </svg>
                 <input
-                  ref={searchRef}
+                  onChange={(e) => setSearch(e.target.value)}
                   className="bg-gray-100 outline-none"
                   type="text"
                   placeholder="Search by name ..."
@@ -70,13 +81,59 @@ const Members = () => {
               {genderLists.map((item, i) => (
                 <Tab
                   key={i}
-                  onClick={() => handleMembers(item)}
+                  onClick={() => handleMembers(item?.value)}
                   className="bg-[#ED0058] hover:bg-[#ed0057b0] text-white capitalize font-bold py-2 px-4 rounded shadow-lg hover:shadow-xl"
                 >
-                  {item}
+                  {item?.label}
                 </Tab>
               ))}
             </TabList>
+            <TabPanel>
+              <div className="grid justify-center items-center gap-2 sm:grid-cols-3 pt-24 pb-4">
+                {members.map((item) => (
+                  <div
+                    className="card w-96 rounded-3xl shadow-xl flex justify-center items-center mx-auto my-16 "
+                    key={item?._id}
+                  >
+                    <div className="p-1 rounded-3xl transform-gpu transition-all selection:bg-sky-100 h-full grid place-items-center bg-gradient-to-tl to-[#FFD3A5] from-[#FD6585] dark:selection:bg-white/10">
+                      <div className="bg-white px-12 pt-16 pb-14 shadow-2xl shadow-black/[0.2] rounded-3xl text-center flex flex-col justify-center max-w-md transition-colors dark:bg-neutral-800">
+                        <div className="select-none">
+                          <img
+                            src={item?.photo}
+                            className="shadow-2xl shadow-black/[0.2] rounded-3xl h-60 w-60 mx-auto -mt-40 transform-gpu transition-all hover:scale-125"
+                            style={{ userSelect: "none" }}
+                            alt="avatar"
+                          />
+                        </div>
+
+                        <h1 className="mt-12 text-3xl font-bold text-slate-800 dark:text-white">
+                          {item?.name}
+                        </h1>
+
+                        <p className="mt-4 text-slate-600 dark:text-white/90">
+                          {item?.bio}
+                        </p>
+                        <p>
+                          <span className="flex justify-center items-center py-3 gap-1 text-sm leading-normal text-[#94A3B8] font-bold uppercase">
+                            <FaMapMarkerAlt />
+                            {item?.location}
+                          </span>
+                        </p>
+
+                        <div className="card-actions">
+                          <a
+                            href={`female/${item._id}`}
+                            className="btn bg-[#FD6585] hover:bg-[#ED0058] w-full"
+                          >
+                            View Details
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </TabPanel>
             {genderLists.map((i) => (
               <TabPanel key={i}>
                 <div className="grid justify-center items-center gap-2 sm:grid-cols-3 pt-24 pb-4">
@@ -112,7 +169,7 @@ const Members = () => {
 
                           <div className="card-actions">
                             <a
-                              href={`female/${female._id}`}
+                              href={`female/${item._id}`}
                               className="btn bg-[#FD6585] hover:bg-[#ED0058] w-full"
                             >
                               View Details
