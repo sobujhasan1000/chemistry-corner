442d8a2ab81dd4873a00b77fa5b32252e9afad82
md mohidul islam <sobujhasan1000@gmail.com>
2023-08-30 17:32:47 +0600

    feadback redsing

--- src/pages/Dashboard/UsersFeedback/UsersFeedback.jsx
+++ src/pages/Dashboard/UsersFeedback/UsersFeedback.jsx
@@ -14,13 +14,71 @@ const UsersFeedback = () => {
       <Helmet>
         <title>Users Feedbacks - Chemistry Corner</title>
       </Helmet>
-      <div className="page-header-bg w-full h-48 md:h-64 bg-no-repeat bg-cover bg-center">
+      <div className="page-header-bg w-full h-48 md:h-64 bg-no-repeat bg-cover bg-center overflow-x-auto">
         <div className="backdrop-blur-lg md:backdrop-blur-xl w-full h-full flex items-center justify-center">
           <h1 className="text-white text-3xl font-bold">Users Feedbacks</h1>
         </div>
       </div>
+      <div className="overflow-x-auto mt-12 px-8 text-black">
+  <table className="table table-zebra ">
+    {/* head */}
+    <thead className="text-xl">
+      <tr>
+        <th>
+          Si No
+        </th>
+        <th>Photo & Name</th>
+        <th>Feedback Type</th>
+        <th>Favorite Color</th>
+        <th>Feedback Rating</th>
+        <th>Feedback Text</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      
+        {feedbacks.map((feedback,index)=>(
+          <tr key={feedback._id}>
+            <td>
+          {index+1}
+        </td>
+        <td>
+          <div className="flex items-center space-x-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src={feedback.image} alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">{feedback.name}</div>
+            </div>
+          </div>
+        </td>
+        <td>
+        {feedback?.feedbackType}
+        </td>
+        <td>{feedback?.feedbackRating} stars</td>
+        <td>{feedback?.feedbackDetails?.slice(0, 20)}...</td>
+        <th>
+          <button title="View" className="rounded-md bg-[#ED0058] hover:bg-white p-2 text-white hover:text-black border border-[#ED0058] transition-all ease-in-out duration-300"><FaRegEye /></button>
+        </th>
+        <td>
+        <button
+                  title="Delete"
+                  className="rounded-md hover:bg-[#ED0058] bg-white p-2 hover:text-white text-black border border-[#ED0058] transition-all ease-in-out duration-300"
+                >
+                  <BiTrash />
+                </button>
+        </td>
+      </tr>
+        ))}
+        
+    </tbody>
+  </table>
+</div>
 
-      <div className="my-20 overflow-x-auto md:max-w-7xl mx-auto ">
+      {/* <div className="my-20 overflow-x-auto md:max-w-7xl mx-auto ">
         <div className="">
           <div className="text-black grid grid-cols-1 md:grid-cols-6">
             <div className="border border-black p-3 text-center">#</div>
@@ -77,7 +135,7 @@ const UsersFeedback = () => {
             </div>
           ))}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
