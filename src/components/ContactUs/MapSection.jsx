a8984223184c623e5166a6bd64b3be733e0880d4
mshipan <mshipan657@gmail.com>
2023-08-30 13:37:22 +0600

    contact us map component error

--- src/components/ContactUs/MapSection.jsx
+++ src/components/ContactUs/MapSection.jsx
@@ -1,14 +1,12 @@
-import Container from "../shared/Container";
-
 const MapSection = () => {
   return (
     <div>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14602.254272231177!2d90.3654215!3d23.7985508!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c105f81691d5%3A0x4aa9bd97de918f7b!2sSher-E-Bangla%20National%20Cricket%20Stadium!5e0!3m2!1sen!2sbd!4v1692264084969!5m2!1sen!2sbd"
         className="w-full h-[60vh] border-0"
-        allowfullscreen=""
+        allowFullScreen=""
         loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
+        referrerPolicy="no-referrer-when-downgrade"
       ></iframe>
     </div>
   );
