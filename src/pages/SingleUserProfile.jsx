5512b9383ce9c3d8c90d4279279bf38f210873e9
simanto97 <saimumsimanto97@gmail.com>
2023-08-31 20:28:14 +0600

    single User profile updated

--- src/pages/SingleUserProfile.jsx
+++ src/pages/SingleUserProfile.jsx
@@ -25,12 +25,12 @@ const SingleUserProfile = () => {
     <div>
       <div className="w-full h-[40vh] flex flex-col items-center justify-center gap-5 bg-[#FD6585]">
         <img
-          src={singleMember.photo}
+          src={singleMember?.image}
           alt="User Image"
           className="w-40 h-40 rounded-full"
         />
-        <h1 className="text-3xl text-black font-signature">
-          {singleMember.name}
+        <h1 className="text-3xl text-black font-signature capitalize">
+          {singleMember?.name}
         </h1>
       </div>
       <div>
@@ -40,8 +40,7 @@ const SingleUserProfile = () => {
               <BsInfoCircle />
               Information
             </h1>
-
-            <p className="text-black">user@gmail.com</p>
+            <p className="text-black">Email: {singleMember?.email}</p>
           </div>
         </div>
         <div className="container mx-auto my-8 flex flex-row">
@@ -55,14 +54,22 @@ const SingleUserProfile = () => {
               <AiOutlineLine className="text-black rotate-90" />
               <h1 className="text-black inline-flex items-center gap-1">
                 <FaUser />
-                {singleMember.age} years old
+                {singleMember?.age} years old
               </h1>
             </div>
-            <p className="text-black">{singleMember.bio}</p>
+            <p className="text-black">{singleMember?.bio}</p>
             <div className="w-full my-10">
               <div className="grid grid-cols-2 border-y border-black py-4">
                 <p className="text-black">I&apos;m a</p>
-                <p className="text-black capitalize">{singleMember.gender}</p>
+                <p
+                  className={`text-black ${
+                    singleMember?.gender && "capitalize"
+                  }`}
+                >
+                  {singleMember?.gender
+                    ? singleMember?.gender
+                    : "Gender is not set yet"}
+                </p>
               </div>
               <div className="grid grid-cols-2 border-b border-black py-4">
                 <p className="text-black">Looking for a</p>
@@ -70,28 +77,61 @@ const SingleUserProfile = () => {
               </div>
               <div className="grid grid-cols-2 border-b border-black py-4">
                 <p className="text-black">Country</p>
-                <p className="text-black">USA</p>
+                <p
+                  className={`text-black ${
+                    singleMember?.country && "capitalize"
+                  }`}
+                >
+                  {singleMember?.country
+                    ? singleMember?.country
+                    : "Country is not set yet"}
+                </p>
               </div>
               <div className="grid grid-cols-2 border-b border-black py-4">
                 <p className="text-black">City</p>
-                <p className="text-black">{singleMember.location}</p>
+                <p
+                  className={`text-black ${singleMember?.city && "capitalize"}`}
+                >
+                  {singleMember?.city
+                    ? singleMember?.city
+                    : "City is not set yet"}
+                </p>
               </div>
               <div className="grid grid-cols-2 border-b border-black py-4">
                 <p className="text-black">Date of Birth</p>
-                <p className="text-black">5/12/1990 (sample data)</p>
+                <p className="text-black">
+                  {singleMember?.dob
+                    ? singleMember?.dob
+                    : "Date of Birth is not set yet"}
+                </p>
               </div>
               <div className="grid grid-cols-2 border-b border-black py-4">
                 <p className="text-black">Marital Status</p>
-                <p className="text-black">Single (sample data)</p>
+                <p
+                  className={`text-black ${
+                    singleMember?.maritalStatus && "capitalize"
+                  }`}
+                >
+                  {singleMember?.maritalStatus
+                    ? singleMember?.maritalStatus
+                    : "Marital status is not set yet"}
+                </p>
               </div>
               <div className="grid grid-cols-2 border-b border-black py-4">
                 <p className="text-black">Education</p>
-                <p className="text-black">BBA (sample data)</p>
+                <p
+                  className={`text-black ${
+                    singleMember?.education && "capitalize"
+                  }`}
+                >
+                  {singleMember?.education
+                    ? singleMember?.education
+                    : "Education is not set yet"}
+                </p>
               </div>
             </div>
             <div>
               <h1 className="text-2xl text-black">Photos</h1>
-
               <div className="flex flex-col md:flex-row items-center gap-2 mt-5">
                 <img src={user1} alt="" className="w-56 h-40" />
                 <img src={user2} alt="" className="w-56 h-40" />
