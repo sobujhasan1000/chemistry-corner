d0dc95cf91fea4247e8ab886960ff3f12457ce1f
moktubat <shadhinreja@gmail.com>
2023-09-04 09:50:08 +0600

    last 3 post show in blog page

--- src/pages/blogs/BlogCategories.jsx
+++ src/pages/blogs/BlogCategories.jsx
@@ -1,7 +1,7 @@
 const BlogCategories = () => {
   return (
     <div>
-      <h1 className="text-center lg:pt-20 text-xl text-black">Categories</h1>
+      <h1 className="text-center lg:pt-20 text-2xl text-black font-bold">Categories</h1>
       <div className="ml-4 lg:ml-20 pl-2 pt-6">
         <h2 className="mb-4 flex items-center justify-between text-gray-500">
           News <span className="inline-block">6</span>
