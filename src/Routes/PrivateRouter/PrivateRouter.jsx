5dd7832306677df3da46940b282056e407cae223
moktubat <shadhinreja@gmail.com>
2023-09-05 01:25:13 +0600

    blog page error fixed

--- src/Routes/PrivateRouter/PrivateRouter.jsx
+++ src/Routes/PrivateRouter/PrivateRouter.jsx
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
-
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
