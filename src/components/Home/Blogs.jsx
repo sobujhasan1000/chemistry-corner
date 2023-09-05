cfeaa9a6f5805a560bd96624beac849329e6bd96
md mohidul islam <sobujhasan1000@gmail.com>
2023-09-05 11:26:50 +0600

    blogs design modify

--- src/components/Home/Blogs.jsx
+++ src/components/Home/Blogs.jsx
@@ -50,13 +50,13 @@ const Blogs = () => {
         />
         <div className="grid grid-cols-1 md:grid-cols-4 gap-10 md:gap-4 ">
           {data.map((item, i) => (
-            <div className="relative group ease-in-out" key={i}>
+            <div className="relative group ease-in-out my-8" key={i}>
               <img
                 className="rounded w-full h-72 z-0 shadow-lg group-hover:scale-105 duration-200"
                 src={item?.image_url}
                 alt=""
               />
-              <div className="bg-white py-6 px-4 rounded z-10 h-36  w-11/12 mx-auto absolute -bottom-8 left-4 md:left-3 space-y-2 group-hover:scale-105 duration-200">
+              <div className="bg-white py-6 px-4 rounded z-10 h-36  w-11/12 mx-auto absolute -bottom-8 left-4 md:left-3 space-y-2 group-hover:scale-105 duration-200 border-2">
                 <h2 className="text-[#212121] font-medium text-[19px] hover:text-[#fe8488] duration-200">
                   {item?.blog_heading}
                 </h2>
