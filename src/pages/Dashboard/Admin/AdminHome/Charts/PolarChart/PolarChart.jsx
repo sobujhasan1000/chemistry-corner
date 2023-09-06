f2a1f4f6ff1312059b41b2dcc7bb4d2814dc2c66
moktubat <shadhinreja@gmail.com>
2023-09-07 00:04:02 +0600

    device user chart fixed

--- src/pages/Dashboard/Admin/AdminHome/Charts/PolarChart/PolarChart.jsx
+++ src/pages/Dashboard/Admin/AdminHome/Charts/PolarChart/PolarChart.jsx
@@ -2,35 +2,50 @@ import ReactApexChart from "react-apexcharts";
 
 const PolarChart = () => {
   const state = {
-    series: [30, 45, 25],
+    series: [45, 25, 30],
     options: {
       chart: {
-        type: 'polarArea',
+        type: "polarArea",
       },
       stroke: {
-        colors: ['#fff']
+        colors: ["#fff"],
       },
       fill: {
-        opacity: 0.8
+        opacity: 0.8,
       },
-      responsive: [{
-        breakpoint: 480,
-        options: {
-          chart: {
-            width: 80
+      labels: ["Mobile", "Tablet", "Desktop"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 80,
+            },
+            legend: {
+              position: "bottom",
+            },
+            labels: ["M", "T", "D"],
           },
-          legend: {
-            position: 'bottom'
-          }
-        }
-      }]
+        },
+        {
+          breakpoint: 768,
+          options: {
+            labels: ["Mobile", "Tablet", "Desktop"],
+          },
+        },
+      ],
     },
   };
 
   return (
     <div>
       <div id="chart">
-        <ReactApexChart options={state.options} series={state.series} type="polarArea" />
+        <h1 className="text-lg font-bold mb-8">Used Devices</h1>
+        <ReactApexChart
+          options={state.options}
+          series={state.series}
+          type="polarArea"
+        />
       </div>
     </div>
   );
