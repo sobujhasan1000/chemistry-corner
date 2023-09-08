5623d2be9c37e2bc00cfee01e6b50bbc66f14f55
moktubat <shadhinreja@gmail.com>
2023-09-08 14:53:43 +0600

    fixing some issue

--- src/pages/Dashboard/GiveAFeedback/GiveAFeedback.jsx
+++ src/pages/Dashboard/GiveAFeedback/GiveAFeedback.jsx
@@ -45,12 +45,14 @@ const GiveAFeedback = () => {
       <Helmet>
         <title>Give a Feedback - Chemistry Corner</title>
       </Helmet>
-      <div className="page-header-bg w-full h-48 md:h-64 bg-no-repeat bg-cover bg-center">
+      {/* <div className="page-header-bg w-full h-48 md:h-64 bg-no-repeat bg-cover bg-center">
         <div className="backdrop-blur-lg md:backdrop-blur-xl w-full h-full flex items-center justify-center">
-          <h1 className="text-white text-3xl font-bold">Give a Feedback</h1>
+          
         </div>
-      </div>
-
+      </div> */}
+      <h1 className="text-black text-center text-3xl font-bold">
+        Give a Feedback
+      </h1>
       <div className="my-20 md:w-3/5 mx-auto">
         <form
           onSubmit={handleSubmit(onSubmit)}
