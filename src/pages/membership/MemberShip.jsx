f8efc35d3575b9067aba6077f17680dab0c7b4b2
moktubat <shadhinreja@gmail.com>
2023-09-14 23:41:37 +0600

    membership color change

--- src/pages/membership/MemberShip.jsx
+++ src/pages/membership/MemberShip.jsx
@@ -2,7 +2,7 @@ import { FaCheckCircle } from "react-icons/fa";
 // import { ImCross } from "react-icons/im";
 import Container from "../../components/shared/Container";
 import { Link } from "react-router-dom";
-import membershipjon from "../../assets/membershipjson.json";
+import membershipjson from "../../assets/membershipjson.json";
 import { Helmet } from "react-helmet-async";
 
 const MemberShip = () => {
@@ -13,8 +13,8 @@ const MemberShip = () => {
       </Helmet>
 
       <Container >
-        <div className="text-black my-10">
-        <div className="text-center  py-6">
+        <div className="text-black my-6">
+        <div className="text-center py-6">
           <h1 className="text-[40px] font-bold text-black">Membership Levels</h1>
           <p>
             Our dating platform is like a breath of fresh air. Clean and trendy
@@ -22,20 +22,20 @@ const MemberShip = () => {
           </p>
         </div>
         <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-4 p-4 py-10">
-          {membershipjon?.map((item, i) => (
+          {membershipjson?.map((item, i) => (
             <div
-              className="gap-2 bg-gradient-to-r from-lime-200 to-pink-400 text-center hover:shadow-2xl rounded-md"
+              className="gap-2 bg-gradient-to-b from-pink-200 to-lime-100 text-center hover:shadow-2xl rounded-lg px-2"
               key={i}
             >
-              <h1 className="text-2xl font-bold py-8">{item.packageName}</h1>
+              <h1 className="text-2xl font-bold pt-6">{item.packageName}</h1>
               {/* <p className="px-10 py-8 bg-[#eca9c2] text-xl">{item.packInfo}</p> */}
-              <h1 className="text-2xl font-bold py-2">${item.price}</h1>
+              <h1 className="text-2xl font-bold pt-2">${item.price}</h1>
               <span className="divider"></span>
-              <ul className="mb-8">
+              <ul className="mb-6">
                 {item.packInclude.map((feature, i) => (
-                  <li className=" text-xl border-b border-gray-400  flex items-center mx-auto w-96 gap-4 py-8 " key={i}>
-                    <FaCheckCircle className="text-blue-400  text-center"  />
-                    <span>{feature} </span>
+                  <li className="text-xl border-b border-gray-400 flex items-center mx-auto w-84 gap-3 py-6" key={i}>
+                    <FaCheckCircle className="text-blue-400 text-center"  />
+                    <span>{feature}</span>
                     {/* <span className="divider px-4"></span> */}
                   </li>
                 ))}
